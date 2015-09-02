f8f66e8b1af22d1bec46be57821cc8f7c759b2ac
Chanho Park <chanho61.park@samsung.com>
2015-09-02 15:19:17 +0900

    build-artik: support release version manipulation
    This patch supports manipulation of release version. We append the
    release version information in the /etc/artik_release of the root file
    system. The basic usage is below:
    
    ./release.sh -c config/artik5.cfg -t GC -n 0E -p 01 -q 0 -r 3
    
    Compositing the parameters, we could generate a full release version
    name like below:
    
    RELEASE_VERSION=A50GC0E-3AF-01030
    
    You can see the help information of the script:
    ./release.sh -h
    -h/--help         Show help options
    -c/--config       Config file path to build ex) -c config/artik5.cfg
    -t/--customer     customer code to release GC:general ST:SmartThings
                      ex) -t GC
    -n/--connectivity Connectivity code, bit 0: Ehternet, bit 1: Wifi
                      bit 2: BT, bit 3: ZigBee, bit 4: ZWave, bit 5: LPWA
                      ex) -n 0E #(Wifi/BT/ZigBee), -n 0F #(Ethernet/Wifi/BT/ZigBee)
    -w                Linux version, 3.10 -> 3A, ex) -w 3A
    -f/--rootfs       Rootfs type, Y: Yocto, F: Fedora, U : Ubuntu, T : Tizen, A : Android
                      ex) -f F # Rootfs is Fedora
    -p/--patchver     SW Patch version - 00 ~ zz, ex) -p 01
    -q/--swqual       SW Qual status, FS:0, ES:E, CS:Q, Test:T
    -r/--rcver        SW RC version, 0~z
    
    Change-Id: Ibef3f89b0acbf85c2b04be153bb44e9da93e8a6b
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- release.sh
+++ release.sh
@@ -1,5 +1,96 @@
 #!/bin/bash
 
+print_usage()
+{
+	echo "-h/--help         Show help options"
+	echo "-c/--config       Config file path to build ex) -c config/artik5.cfg"
+	echo "-t/--customer     customer code to release GC:general ST:SmartThings"
+	echo "                  ex) -t GC"
+	echo "-n/--connectivity Connectivity code, bit 0: Ehternet, bit 1: Wifi"
+	echo "                  bit 2: BT, bit 3: ZigBee, bit 4: ZWave, bit 5: LPWA"
+	echo "                  ex) -n 0E #(Wifi/BT/ZigBee), -n 0F #(Ethernet/Wifi/BT/ZigBee)"
+	echo "-w                Linux version, 3.10 -> 3A, ex) -w 3A"
+	echo "-f/--rootfs       Rootfs type, Y: Yocto, F: Fedora, U : Ubuntu, T : Tizen, A : Android"
+	echo "                  ex) -f F # Rootfs is Fedora"
+	echo "-p/--patchver     SW Patch version - 00 ~ zz, ex) -p 01"
+	echo "-q/--swqual       SW Qual status, FS:0, ES:E, CS:Q, Test:T"
+	echo "-r/--rcver        SW RC version, 0~z"
+	exit 0
+}
+
+parse_options()
+{
+	for opt in "$@"
+	do
+		case "$opt" in
+			-h|--help)
+				print_usage
+				shift ;;
+			-c|--config)
+				CONFIG_FILE="$2"
+				shift ;;
+			-t|--customer)
+				CUSTOMER_CODE="$2"
+				shift ;;
+			-n|--connectivity)
+				CONNECTIVITY_CODE="$2"
+				shift ;;
+			-f|--rootfs)
+				ROOTFS_TYPE="$2"
+				shift ;;
+			-w|--swver)
+				SW_VERSION="$2"
+				shift ;;
+			-p|--patchver)
+				PATCH_VERSION="$2"
+				shift ;;
+			-q|--swqual)
+				SW_QUAL="$2"
+				shift ;;
+			-r|--rcver)
+				RC_VER="$2"
+				shift ;;
+			*)
+				shift ;;
+		esac
+	done
+}
+
+make_release_version()
+{
+	if [ "$CUSTOMER_CODE" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:3}${CUSTOMER_CODE}${RELEASE_VER:5}
+	fi
+	if [ "$CONNECTIVITY_CODE" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:5}${CONNECTIVITY_CODE}${RELEASE_VER:7}
+	fi
+	if [ "$SW_VERSION" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:8}${SW_VERSION}${RELEASE_VER:10}
+	fi
+	if [ "$ROOTFS_TYPE" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:10}${ROOTFS_TYPE}${RELEASE_VER:11}
+	fi
+	if [ "$PATCH_VERSION" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:12}${PATCH_VERSION}${RELEASE_VER:14}
+	fi
+	if [ "$SW_QUAL" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:14}${SW_QUAL}${RELEASE_VER:15}
+	fi
+	if [ "$RC_VER" != "" ]; then
+		RELEASE_VER=${RELEASE_VER:0:15}${RC_VER}${RELEASE_VER:16}
+	fi
+	export RELEASE_VER=$RELEASE_VER
+}
+
+parse_options "$@"
+
+if [ "$CONFIG_FILE" != "" ]
+then
+	. $CONFIG_FILE
+fi
+
+make_release_version
+
 export RELEASE_DATE=`date +"%Y%m%d.%H%M%S"`
 TARGET_DIR_BACKUP=$TARGET_DIR
 export TARGET_DIR=$TARGET_DIR/$RELEASE_DATE
