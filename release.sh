781c7dd599151d47b0ac230b94017f8f838bdb67
Chanho Park <chanho61.park@samsung.com>
2015-09-25 11:42:11 +0900

    build-artik: simplify release.sh script
    This patch removes unnecessary options of the release.sh script
    because we only use the -v option instead of individuall options.
    If the version infomation is not specified, the result directory
    will not include the version name.
    
    Change-Id: I73fe52465c816105a14aa5ad793bb30ae40aad1b
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- release.sh
+++ release.sh
@@ -4,17 +4,6 @@ print_usage()
 {
 	echo "-h/--help         Show help options"
 	echo "-c/--config       Config file path to build ex) -c config/artik5.cfg"
-	echo "-t/--customer     customer code to release GC:general ST:SmartThings"
-	echo "                  ex) -t GC"
-	echo "-n/--connectivity Connectivity code, bit 0: Ehternet, bit 1: Wifi"
-	echo "                  bit 2: BT, bit 3: ZigBee, bit 4: ZWave, bit 5: LPWA"
-	echo "                  ex) -n 0E #(Wifi/BT/ZigBee), -n 0F #(Ethernet/Wifi/BT/ZigBee)"
-	echo "-w                Linux version, 3.10 -> 3A, ex) -w 3A"
-	echo "-f/--rootfs       Rootfs type, Y: Yocto, F: Fedora, U : Ubuntu, T : Tizen, A : Android"
-	echo "                  ex) -f F # Rootfs is Fedora"
-	echo "-p/--patchver     SW Patch version - 00 ~ zz, ex) -p 01"
-	echo "-q/--swqual       SW Qual status, FS:0, ES:E, CS:Q, Test:T"
-	echo "-r/--rcver        SW RC version, 0~z"
 	echo "-v/--fullver      Pass full version name like: -v A50GC0E-3AF-01030"
 	echo "-d/--date		Release date: -d 20150911.112204"
 	echo "-m/--microsd	Make a microsd bootable image"
@@ -32,29 +21,8 @@ parse_options()
 			-c|--config)
 				CONFIG_FILE="$2"
 				shift ;;
-			-t|--customer)
-				CUSTOMER_CODE="$2"
-				shift ;;
-			-n|--connectivity)
-				CONNECTIVITY_CODE="$2"
-				shift ;;
-			-f|--rootfs)
-				ROOTFS_TYPE="$2"
-				shift ;;
-			-w|--swver)
-				SW_VERSION="$2"
-				shift ;;
-			-p|--patchver)
-				PATCH_VERSION="$2"
-				shift ;;
-			-q|--swqual)
-				SW_QUAL="$2"
-				shift ;;
-			-r|--rcver)
-				RC_VER="$2"
-				shift ;;
 			-v|--fullver)
-				REL_VER="$2"
+				RELEASE_VER="$2"
 				shift ;;
 			-d|--date)
 				RELEASE_DATE="$2"
@@ -68,43 +36,12 @@ parse_options()
 	done
 }
 
-make_release_version()
-{
-	if [ "$CUSTOMER_CODE" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:3}${CUSTOMER_CODE}${RELEASE_VER:5}
-	fi
-	if [ "$CONNECTIVITY_CODE" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:5}${CONNECTIVITY_CODE}${RELEASE_VER:7}
-	fi
-	if [ "$SW_VERSION" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:8}${SW_VERSION}${RELEASE_VER:10}
-	fi
-	if [ "$ROOTFS_TYPE" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:10}${ROOTFS_TYPE}${RELEASE_VER:11}
-	fi
-	if [ "$PATCH_VERSION" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:12}${PATCH_VERSION}${RELEASE_VER:14}
-	fi
-	if [ "$SW_QUAL" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:14}${SW_QUAL}${RELEASE_VER:15}
-	fi
-	if [ "$RC_VER" != "" ]; then
-		RELEASE_VER=${RELEASE_VER:0:15}${RC_VER}${RELEASE_VER:16}
-	fi
-	if [ "$REL_VER" != "" ]; then
-		RELEASE_VER=$REL_VER
-	fi
-	export RELEASE_VER=$RELEASE_VER
-}
-
 parse_options "$@"
 
 if [ "$CONFIG_FILE" != "" ]
 then
 	. $CONFIG_FILE
 fi
-
-make_release_version
 
 if [ "$RELEASE_DATE" == "" ]
 then
@@ -113,6 +50,7 @@ fi
 
 export RELEASE_DATE=$RELEASE_DATE
 TARGET_DIR_BACKUP=$TARGET_DIR
+
 export TARGET_DIR=$TARGET_DIR/$RELEASE_VER/$RELEASE_DATE
 
 sudo ls > /dev/null 2>&1
