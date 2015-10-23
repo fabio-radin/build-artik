5b35958e696016865fa029dd462e3817960fd25a
Chanho Park <chanho61.park@samsung.com>
2015-10-23 13:21:01 +0900

    build_kernel: build kernel modules
    This patch enables kernel modules build and copy the modules.img
    into output directory.
    
    Change-Id: Ica13581bf04abfde85d286872d46e09379cc7a95
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,9 +11,12 @@ make $KERNEL_DEFCONFIG
 make zImage -j$JOBS
 make $KERNEL_DTB
 
+./scripts/mk_modules.sh
+
 cp arch/arm/boot/zImage $TARGET_DIR
 cp arch/arm/boot/dts/$KERNEL_DTB $TARGET_DIR
 cp vmlinux $TARGET_DIR
+cp usr/modules.img $TARGET_DIR
 
 KERNEL_VERSION=`make ARCH=arm kernelrelease`
 
