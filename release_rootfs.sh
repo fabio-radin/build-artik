6450f9f3685339325b6236f70894aba86d5a054c
Chanho Park <chanho61.park@samsung.com>
2015-10-23 14:40:26 +0900

    release_rootfs: support downloading rootfs
    This patch changes the rootfs from pixeom rootfs to fedora22.
    If the rootfs is not founded, it will be automatically downloaded
    from web server.
    
    Change-Id: Ic30b4dcf7d46c2e9d25247119fe75951af250611
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- release_rootfs.sh
+++ release_rootfs.sh
@@ -3,56 +3,10 @@
 set -x
 
 test -d ${TARGET_DIR} || mkdir -p ${TARGET_DIR}
-test -d ${TMP_DIR} || mkdir -p ${TMP_DIR}
 
-if [ -z "$RELEASE_DATE" ]; then
-	RELEASE_DATE=`date +"%Y%m%d.%H%M%S"`
+if [ ! -f $PREBUILT_DIR/$ROOTFS_FILE ]; then
+	echo "Not found rootfs. Just download it"
+	wget http://artik:artik%40iot@dage3.mooo.com/downloads/artik/fedora/$ROOTFS_FILE -O $PREBUILT_DIR/$ROOTFS_FILE
 fi
 
-pushd ${TMP_DIR}
-
-test -e rootfs_tmp || mkdir rootfs_tmp
-
-sudo rm -rf rootfs_tmp/*
-sudo tar xf $ROOTFS_FILE -C rootfs_tmp
-
-pushd rootfs_tmp
-
-for dir in $ROOTFS_ATTACH_DIRS
-do
-	sudo cp -rf $dir/* ./
-done
-
-# Fix rootfs failed service
-sudo sed -i 's/kernel.pid_max = 65536/kernel.pid_max = 32768/g' etc/sysctl.conf
-
-sudo mkdir var/log/sa
-
-# Remove unnecessary services
-sudo rm etc/systemd/system/multi-user.target.wants/cups.path
-sudo rm etc/systemd/system/sockets.target.wants/cups.socket
-sudo rm etc/systemd/system/multi-user.target.wants/sendmail.service
-sudo rm etc/systemd/system/multi-user.target.wants/sm-client.service
-
-# Remove invalid openstack repo
-sudo rm etc/yum.repos.d/rdo-release.repo
-
-# Use mirror repo for updates
-sudo sed -i "s/baseurl=/#baseurl=/g" etc/yum.repos.d/fedora.repo
-sudo sed -i "s/#metalink=/metalink=/g" etc/yum.repos.d/fedora.repo
-
-sudo sed -i "s/baseurl=/#baseurl=/g" etc/yum.repos.d/fedora-updates.repo
-sudo sed -i "s/#metalink=/metalink=/g" etc/yum.repos.d/fedora-updates.repo
-
-# Allow to access ssh for other users
-sudo sed -i "/AllowUsers /d" etc/ssh/sshd_config
-
-sudo cp ${TARGET_DIR}/artik_release etc/
-
-sudo tar zcf ${TARGET_DIR}/rootfs.tar.gz *
-
-popd
-
-sudo rm -rf rootfs_tmp
-
-popd
+cp $PREBUILT_DIR/$ROOTFS_FILE $TARGET_DIR/rootfs.tar.gz
