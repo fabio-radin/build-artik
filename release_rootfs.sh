f5181e953fb5ae2dc600fe757d14835fec0364fa
Chanho Park <chanho61.park@samsung.com>
2016-02-17 15:49:23 +0900

    fedora: use ipaddr instead of domain
    The domain was blocked by firewall. Thus, we need to use
    the ip address directly.
    
    Change-Id: Ie9a013f6b65f57cf284a4dc9c80aa2ac4da64014
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- release_rootfs.sh
+++ release_rootfs.sh
@@ -6,7 +6,7 @@ test -d ${TARGET_DIR} || mkdir -p ${TARGET_DIR}
 
 if [ ! -f $PREBUILT_DIR/$ROOTFS_FILE ]; then
 	echo "Not found rootfs. Just download it"
-	wget http://artik:artik%40iot@dage3.mooo.com/downloads/artik/fedora/$ROOTFS_FILE -O $PREBUILT_DIR/$ROOTFS_FILE
+	wget http://artik:artik%40iot@59.13.55.140/downloads/artik/fedora/$ROOTFS_FILE -O $PREBUILT_DIR/$ROOTFS_FILE
 fi
 
 cp $PREBUILT_DIR/$ROOTFS_FILE $TARGET_DIR/rootfs.tar.gz
