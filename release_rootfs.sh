e43ce9a43489f0a67b95e7ef7f38edcb80de8cd1
Chanho Park <chanho61.park@samsung.com>
2016-04-16 13:19:25 +0900

    rootfs: compare md5 checksum
    This patch adds md5 checksum comparison to verify fedora
    rootfs which is downloaded from server.
    
    Change-Id: If54155d7c3a027064097b8e42b7efe0645c98360
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- release_rootfs.sh
+++ release_rootfs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -x
+set -e
+
+CHECK_COUNT=0
+MAX_RETRY=3
 
 test -d ${TARGET_DIR} || mkdir -p ${TARGET_DIR}
 
@@ -8,5 +12,22 @@ if [ ! -f $PREBUILT_DIR/$ROOTFS_FILE ]; then
 	echo "Not found rootfs. Just download it"
 	wget http://artik:artik%40iot@59.13.55.140/downloads/artik/fedora/$ROOTFS_FILE -O $PREBUILT_DIR/$ROOTFS_FILE
 fi
+
+while :
+do
+	MD5_SUM=$(md5sum $PREBUILT_DIR/$ROOTFS_FILE | awk '{print $1}')
+	if [ "$ROOTFS_FILE_MD5" == "$MD5_SUM" ]; then
+		break
+	fi
+
+	echo "Mismatch MD5 hash. Just download again"
+	wget http://artik:artik%40iot@59.13.55.140/downloads/artik/fedora/$ROOTFS_FILE -O $PREBUILT_DIR/$ROOTFS_FILE
+
+	CHECK_COUNT=$((CHECK_COUNT + 1))
+
+	if [ $CHECK_COUNT -ge $MAX_RETRY ]; then
+		exit -1
+	fi
+done
 
 cp $PREBUILT_DIR/$ROOTFS_FILE $TARGET_DIR/rootfs.tar.gz
