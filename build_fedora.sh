fabio-radin/build-artik

commit aac9020aa1625c85b4396e3902c9cf6fc75e2749

fedora: support to specify fedora package file
This patch adds a support to specify where the fedora
packge file is located. If "BASE_BOARD" is defined,
it will use BASE_BOARD's package file.

Change-Id: I81c2e5dda7c81a2adbba36414eb9e70745af88e6
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

build_fedora.sh
@@ -4,7 +4,8 @@ set -x
 
 TARGET_DIR=$1
 TARGET_BOARD=$2
-FEDORA_NAME=$3
+TARGET_PACKAGE=$3
+FEDORA_NAME=$4
 
 package_check()
 {
@@ -23,7 +24,7 @@ build_package()
 
 package_check fed-artik-creator
 
-FEDORA_PACKAGES=`cat config/artik5_fedora.package`
+FEDORA_PACKAGES=`cat $TARGET_PACKAGE`
 
 echo "Clean up local repository..."
 fed-artik-build --clean-repos-and-exit

release.sh
@@ -143,8 +143,22 @@ fi
 ./mkbootimg.sh
 
 if $FULL_BUILD ; then
+	set +e
+	if [ "$BASE_BOARD" != "" ]; then
+		FEDORA_TARGET_BOARD=$BASE_BOARD
+		if [ -e config/${TARGET_BOARD}.package ]; then
+			FEDORA_PACKAGE_FILE=config/${TARGET_BOARD}_fedora.package
+		else
+			FEDORA_PACKAGE_FILE=config/${BASE_BOARD}_fedora.package
+		fi
+	else
+		FEDORA_TARGET_BOARD=$TARGET_BOARD
+		FEDORA_PACKAGE_FILE=config/${TARGET_BOARD}_fedora.package
+	fi
+	set -e
+
 	FEDORA_NAME=fedora-arm-artik-rootfs-$RELEASE_VER-$RELEASE_DATE
-	./build_fedora.sh $TARGET_DIR $TARGET_BOARD $FEDORA_NAME
+	./build_fedora.sh $TARGET_DIR $FEDORA_TARGET_BOARD $FEDORA_PACKAGE_FILE $FEDORA_NAME
 	FEDORA_TARBALL=${FEDORA_NAME}.tar.gz
 	cp $TARGET_DIR/$FEDORA_TARBALL $TARGET_DIR/rootfs.tar.gz
 else

commit c724a95282152fce49b615b8f875a59fd71cb98b

fedora: fix bug when target package file doesn't exist
This patch fixes bug when target package file doesn't exist.
If target package file doesn't exist, cat 'blank' and it makes
script hang. So this patch makes that 'cat $TARGET_PACKAGE' line
is only excuted when --skip-build option doesn't exist.

Fixes: aac9020 ("fedora: support to specify fedora package file")

Change-Id: I45ad7be8404ffcb09b83bcc8bf35a619721be2c4
Signed-off-by: Sowon Na <sowon.na@samsung.com>

build_fedora.sh
@@ -95,8 +95,6 @@ else
 	BUILD_CONF=""
 fi
 
-FEDORA_PACKAGES=`cat $TARGET_PACKAGE`
-
 if ! $SKIP_CLEAN; then
 	echo "Clean up local repository..."
 	fed-artik-build $BUILD_CONF --clean-repos-and-exit
@@ -108,6 +106,8 @@ if [ "$PREBUILT_RPM_DIR" != "" ]; then
 fi
 
 if ! $SKIP_BUILD; then
+	FEDORA_PACKAGES=`cat $TARGET_PACKAGE`
+
 	for pkg in $FEDORA_PACKAGES
 	do
 		build_package $pkg