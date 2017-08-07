fabio-radin/build-artik

commit bb4aa838642d13f57697c97ca0ba4aee0a3cae45

support ubuntu full build
This patch adds to support ubuntu full build.
You can build the ubuntu like this:
./release.sh -c config/artik530.cfg --full-build --ubuntu
It will build all packages from config/artik530_ubuntu.package
and launch local repository server with random port.

Change-Id: I4402cd8f797ab1f20a88ae156222585f88638b48
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

build_ubuntu.sh
@@ -1,0 +1,228 @@
+#!/bin/bash
+
+set -e
+
+TARGET_PACKAGE=
+BUILDCONFIG=
+ARCH=armhf
+SBUILD_CONF=~/.sbuildrc
+DEST_DIR=
+PORT=
+SKIP_BUILD=false
+PREBUILT_DIR=
+IMG_DIR=
+UBUNTU_NAME=
+PREBUILT_REPO_DIR=
+
+print_usage()
+{
+	echo "-h/--help         Show help options"
+	echo "-p|--package	Target package file"
+	echo "-A|--arch		Target architecture(ex: armhf, arm64)"
+	echo "--chroot		Chroot name"
+	echo "-C|--sbuild-conf	Sbuild configuration path"
+	echo "-D|--dest-dir	Build output directory"
+	echo "-s|--server-port	Server port"
+	echo "--skip-build	Skip package build"
+	echo "--prebuilt-dir	Skip package build"
+	echo "--use-prebuilt-repo	Use prebuilt repository"
+	echo "--img-dir		Image generation directory"
+	echo "-n|--ubuntu-name	Ubuntu image name"
+	exit 0
+}
+
+parse_options()
+{
+	for opt in "$@"
+	do
+		case "$opt" in
+			-h|--help)
+				print_usage
+				shift ;;
+			-A|--arch)
+				ARCH="$2"
+				shift ;;
+			-p|--package)
+				TARGET_PACKAGE=`readlink -e "$2"`
+				shift ;;
+			--chroot)
+				CHROOT="$2"
+				shift ;;
+			-C|--sbuild-conf)
+				SBUILD_CONF=`readlink -e "$2"`
+				shift ;;
+			-D|--dest-dir)
+				DEST_DIR=`readlink -e "$2"`
+				shift ;;
+			-s|--server-port)
+				PORT="$2"
+				shift ;;
+			--skip-build)
+				SKIP_BUILD=true
+				shift ;;
+			--prebuilt-dir)
+				PREBUILT_DIR=`readlink -e "$2"`
+				shift ;;
+			--use-prebuilt-repo)
+				PREBUILT_REPO_DIR=`readlink -e "$2"`
+				shift ;;
+			--img-dir)
+				IMG_DIR=`readlink -e "$2"`
+				shift ;;
+			-n|--ubuntu-name)
+				UBUNTU_NAME="$2"
+				shift ;;
+			*)
+				shift ;;
+		esac
+	done
+}
+
+package_check()
+{
+	command -v $1 >/dev/null 2>&1 || { echo >&2 "${1} not installed. Aborting."; exit 1; }
+}
+
+gen_ubuntu_meta()
+{
+	where=$(readlink -e $1)
+	origin=$2
+	label=$3
+
+	pushd $where
+	apt-ftparchive sources . \
+		| tee "$where"/Sources \
+		| gzip -9 > "$where"/Sources.gz
+
+	apt-ftparchive packages "$where" \
+		| sed "s@$where@@" \
+		| tee "$where"/Packages \
+		| gzip -9 > "$where"/Packages.gz
+
+	# sponge comes from moreutils
+	apt-ftparchive \
+		-o"APT::FTPArchive::Release::Origin=$origin" \
+		-o"APT::FTPArchive::Release::Label=$label" \
+		-o"APT::FTPArchive::Release::Codename=$where" release "$where" \
+		| sponge "$where"/Release
+	popd
+}
+
+move_build_output()
+{
+	where=$(readlink -e $1)
+	set +e
+	mv *.build *.changes *.dsc *.deb *.ddeb *.tar.* *.udeb $where
+}
+
+start_local_server()
+{
+	where=$(readlink -e $1)
+	port=$2
+
+	pushd $where
+	gen_ubuntu_meta $where artik-local repo
+	python3 -m http.server $port --bind 127.0.0.1&
+	SERVER_PID=$!
+	popd
+}
+
+stop_local_server()
+{
+	kill -9 $SERVER_PID
+}
+
+build_package()
+{
+	local pkg=$1
+	local dest_dir=$2
+
+	if [ -d $pkg ]; then
+		debian_dir=`find ./$pkg -name "debian" -type d`
+		if [ -d $debian_dir ]; then
+			pushd $debian_dir/../
+			echo "Build $pkg.."
+			SBUILD_CONFIG=$SBUILD_CONF sbuild --chroot $CHROOT \
+				--host $ARCH \
+				--extra-repository="deb [trusted=yes] http://localhost:$PORT ./" \
+				--anything-failed-commands="touch $dest_dir/.build_failed" \
+				-j$JOBS
+			popd
+			move_build_output $dest_dir
+			gen_ubuntu_meta $dest_dir artik-local repo
+			if [ -e $dest_dir/.build_failed ]; then
+				abnormal_exit
+				exit -1
+			fi
+		fi
+	fi
+}
+
+abnormal_exit()
+{
+	if [ "$SERVER_PID" != "" ]; then
+		kill -9 $SERVER_PID
+	fi
+	if [ -e "${DEST_DIR}/debs/.build_failed" ]; then
+		rm -f $DEST_DIR/debs/.build_failed
+	fi
+}
+
+find_unused_port()
+{
+	read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
+	while :
+	do
+		PORT="`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
+		ss -lpn | grep -q ":$PORT " || break
+	done
+}
+
+trap abnormal_exit INT ERR
+
+package_check sbuild sponge python3
+
+parse_options "$@"
+
+if [ "$PORT" == "" ]; then
+	find_unused_port
+fi
+
+UBUNTU_PACKAGES=`cat $TARGET_PACKAGE`
+
+[ -d $DEST_DIR/debs ] || mkdir -p $DEST_DIR/debs
+
+if [ "$PREBUILT_REPO_DIR" != "" ]; then
+	cp -rf $PREBUILT_REPO_DIR/* $DEST_DIR/debs
+fi
+
+start_local_server $DEST_DIR/debs $PORT
+
+pushd ../
+
+if ! $SKIP_BUILD; then
+	for pkg in $UBUNTU_PACKAGES
+	do
+		build_package $pkg $DEST_DIR/debs
+	done
+fi
+
+popd
+
+if [ "$PREBUILT_DIR" != "" ]; then
+	echo "Copy prebuilt packages"
+	cp -f $PREBUILT_DIR/*.deb $DEST_DIR/debs
+	gen_ubuntu_meta $DEST_DIR/debs artik-local repo
+fi
+
+if [ "$IMG_DIR" != "" ]; then
+	echo "An ubuntu image generation starting..."
+	pushd $IMG_DIR
+	make clean
+	PORT=$PORT ./configure
+	make IMAGEPREFIX=$UBUNTU_NAME
+	mv $UBUNTU_NAME* $DEST_DIR
+	echo "A new ubuntu image has been created"
+fi
+
+stop_local_server

config/artik10_ubuntu.package
@@ -1,0 +1,16 @@
+bluez
+wpa_supplicant
+connman
+libdrm
+coap
+lwm2m
+iotivity
+mqttsn
+mqttsn_gateway
+wayland-protocols
+wayland
+weston
+xf86-video-armsoc
+artik-plugin
+artik-plugin-artik10
+artik-ubuntu-meta

config/artik530_ubuntu.package
@@ -1,0 +1,28 @@
+bluez
+wpa_supplicant
+connman
+libdrm
+nx-drm-allocator
+nx-renderer
+nx-scaler
+nx-v4l2
+nx-gst-meta
+nx-video-api
+gst-plugins-camera-s5p6818
+gst-plugins-renderer
+gst-plugins-scaler
+gst-plugins-video-dec
+gst-plugins-video-enc
+gst-plugins-video-sink
+coap
+lwm2m
+iotivity
+mqttsn
+mqttsn_gateway
+wayland-protocols
+wayland
+weston
+xf86-video-armsoc
+artik-plugin
+artik-plugin-artik530
+artik-ubuntu-meta

config/artik5_ubuntu.package
@@ -1,0 +1,16 @@
+bluez
+wpa_supplicant
+connman
+libdrm
+coap
+lwm2m
+iotivity
+mqttsn
+mqttsn_gateway
+wayland-protocols
+wayland
+weston
+xf86-video-armsoc
+artik-plugin
+artik-plugin-artik5
+artik-ubuntu-meta

config/artik710_ubuntu.package
@@ -1,0 +1,28 @@
+bluez
+wpa_supplicant
+connman
+libdrm
+nx-drm-allocator
+nx-renderer
+nx-scaler
+nx-v4l2
+nx-gst-meta
+nx-video-api
+gst-plugins-camera-s5p6818
+gst-plugins-renderer
+gst-plugins-scaler
+gst-plugins-video-dec
+gst-plugins-video-enc
+gst-plugins-video-sink
+coap
+lwm2m
+iotivity
+mqttsn
+mqttsn_gateway
+wayland-protocols
+wayland
+weston
+xf86-video-armsoc
+artik-plugin
+artik-plugin-artik710
+artik-ubuntu-meta

release.sh
@@ -9,9 +9,12 @@ VBOOT_KEYDIR=
 VBOOT_ITS=
 SKIP_CLEAN=
 SKIP_FEDORA_BUILD=
+SKIP_UBUNTU_BUILD=
 BUILD_CONF=
 PREBUILT_VBOOT_DIR=
+PREBUILT_REPO_OPT=
 DEPLOY=false
+OS_NAME=fedora
 
 print_usage()
 {
@@ -23,6 +26,7 @@ print_usage()
 	echo "-u/--url		Specify an url for downloading rootfs"
 	echo "-C		fed-artik-build configuration file"
 	echo "--full-build	Full build with generating fedora rootfs"
+	echo "--ubuntu		Ubuntu rootfs build"
 	echo "--local-rootfs	Copy fedora rootfs from local file instead of downloading"
 	echo "--vboot		Generated verified boot image"
 	echo "--vboot-keydir	Specify key directoy for verified boot"
@@ -30,6 +34,8 @@ print_usage()
 	echo "--sboot		Generated signed boot image"
 	echo "--skip-clean	Skip fedora local repository clean"
 	echo "--skip-fedora-build	Skip fedora build"
+	echo "--use-prebuilt-repo	Use prebuilt repository"
+	echo "--skip-ubuntu-build	Skip ubuntu build"
 	echo "--prebuilt-vboot	Specify prebuilt directory path for vboot"
 	echo "--deploy-all	Deploy release"
 	exit 0
@@ -94,11 +100,20 @@ parse_options()
 			--skip-fedora-build)
 				SKIP_FEDORA_BUILD=--skip-build
 				shift ;;
+			--skip-ubuntu-build)
+				SKIP_UBUNTU_BUILD=--skip-build
+				shift ;;
+			--use-prebuilt-repo)
+				PREBUILT_REPO_OPT="--use-prebuilt-repo $2"
+				shift ;;
 			--prebuilt-vboot)
 				PREBUILT_VBOOT_DIR=`readlink -e "$2"`
 				shift ;;
 			--deploy-all)
 				DEPLOY=true
+				shift ;;
+			--ubuntu)
+				OS_NAME=ubuntu
 				shift ;;
 			*)
 				shift ;;
@@ -209,24 +224,39 @@ fi
 
 if $FULL_BUILD ; then
 	if [ "$BASE_BOARD" != "" ]; then
-		FEDORA_TARGET_BOARD=$BASE_BOARD
+		OS_TARGET_BOARD=$BASE_BOARD
 	else
-		FEDORA_TARGET_BOARD=$TARGET_BOARD
+		OS_TARGET_BOARD=$TARGET_BOARD
 	fi
 
-	FEDORA_NAME=fedora-arm-$FEDORA_TARGET_BOARD-rootfs-$BUILD_VERSION-$BUILD_DATE
-	if [ "$FEDORA_PREBUILT_RPM_DIR" != "" ]; then
-		PREBUILD_ADD_CMD="-r $FEDORA_PREBUILT_RPM_DIR"
+	OS_OUTPUT_NAME=${OS_NAME}-arm-$OS_TARGET_BOARD-rootfs-$BUILD_VERSION-$BUILD_DATE
+	if [ "$OS_NAME" == "ubuntu" ]; then
+		BUILD_ARCH=$ARCH
+		if [ "$BUILD_ARCH" == "arm" ]; then
+			BUILD_ARCH=armhf
+		fi
+		UBUNTU_IMG_DIR=../ubuntu-build-service/xenial-${BUILD_ARCH}-${OS_TARGET_BOARD}
+		./build_ubuntu.sh -p config/${OS_TARGET_BOARD}_ubuntu.package \
+			--ubuntu-name $OS_OUTPUT_NAME \
+			$PREBUILT_REPO_OPT \
+			--arch $BUILD_ARCH --chroot xenial-amd64-${BUILD_ARCH} \
+			--dest-dir $TARGET_DIR $SKIP_UBUNTU_BUILD \
+			--prebuilt-dir ../ubuntu-build-service/prebuilt/$BUILD_ARCH \
+			--img-dir $UBUNTU_IMG_DIR
+	else
+		if [ "$FEDORA_PREBUILT_RPM_DIR" != "" ]; then
+			PREBUILD_ADD_CMD="-r $FEDORA_PREBUILT_RPM_DIR"
+		fi
+		./build_fedora.sh $BUILD_CONF -o $TARGET_DIR -b $OS_TARGET_BOARD \
+			-p $FEDORA_PACKAGE_FILE -n $OS_OUTPUT_NAME $SKIP_CLEAN $SKIP_FEDORA_BUILD \
+			-k fedora-arm-${OS_TARGET_BOARD}.ks \
+			$PREBUILD_ADD_CMD
 	fi
-	./build_fedora.sh $BUILD_CONF -o $TARGET_DIR -b $FEDORA_TARGET_BOARD \
-		-p $FEDORA_PACKAGE_FILE -n $FEDORA_NAME $SKIP_CLEAN $SKIP_FEDORA_BUILD \
-		-k fedora-arm-${FEDORA_TARGET_BOARD}.ks \
-		$PREBUILD_ADD_CMD
 
-	MD5_SUM=$(md5sum $TARGET_DIR/${FEDORA_NAME}.tar.gz | awk '{print $1}')
-	FEDORA_TARBALL=${FEDORA_NAME}-${MD5_SUM}.tar.gz
-	mv $TARGET_DIR/${FEDORA_NAME}.tar.gz $TARGET_DIR/$FEDORA_TARBALL
-	cp $TARGET_DIR/$FEDORA_TARBALL $TARGET_DIR/rootfs.tar.gz
+	MD5_SUM=$(md5sum $TARGET_DIR/${OS_OUTPUT_NAME}.tar.gz | awk '{print $1}')
+	OS_TARBALL=${OS_OUTPUT_NAME}-${MD5_SUM}.tar.gz
+	mv $TARGET_DIR/${OS_OUTPUT_NAME}.tar.gz $TARGET_DIR/$OS_TARBALL
+	cp $TARGET_DIR/$OS_TARBALL $TARGET_DIR/rootfs.tar.gz
 else
 	if [ "$LOCAL_ROOTFS" == "" ]; then
 		./release_rootfs.sh -b $TARGET_BOARD $SERVER_URL

commit 275c16df268ea5defe122e4533e3b30e0e160dad

ubuntu: fix bug when target package file doesn't exist
This patch fixes bug when target package file doesn't exist.
If target package file doesn't exist, cat 'blank' and it makes
script hang. So this patch makes that 'cat $TARGET_PACKAGE' line
is only executed when --skip-build option doesn't exist.

Fixes: bb4aa83 ("support ubuntu full build")

Change-Id: Id014888c526db4d558564d5eb6444ddf3aa25280
Signed-off-by: Sowon Na <sowon.na@samsung.com>

build_ubuntu.sh
@@ -191,8 +191,6 @@ if [ "$PORT" == "" ]; then
 	find_unused_port
 fi
 
-UBUNTU_PACKAGES=`cat $TARGET_PACKAGE`
-
 [ -d $DEST_DIR/debs ] || mkdir -p $DEST_DIR/debs
 
 if [ "$PREBUILT_REPO_DIR" != "" ]; then
@@ -204,6 +202,8 @@ start_local_server $DEST_DIR/debs $PORT
 pushd ../
 
 if ! $SKIP_BUILD; then
+	UBUNTU_PACKAGES=`cat $TARGET_PACKAGE`
+
 	for pkg in $UBUNTU_PACKAGES
 	do
 		build_package $pkg $DEST_DIR/debs