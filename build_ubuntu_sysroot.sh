330208324f170981b69c52b531920a838820d75f
Sowon Na <sowon.na@samsung.com>
2017-11-01 11:14:10 +0900

    sysroot: modify help message for --prebuilt-dir
    This patch fixes wrong help message for --prebuilt-dir.
    
    Change-Id: Icdbb2428b2f593032244a05db43799712f954ebe
    Signed-off-by: Sowon Na <sowon.na@samsung.com>

--- build_ubuntu_sysroot.sh
+++ build_ubuntu_sysroot.sh
@@ -26,7 +26,7 @@ print_usage()
 	echo "-D|--dest-dir	Build output directory"
 	echo "-s|--server-port	Server port"
 	echo "--skip-build	Skip package build"
-	echo "--prebuilt-dir	Skip package build"
+	echo "--prebuilt-dir	Specify a directory which contains prebuilt debs"
 	echo "--prebuilt-module-dir     Specify a directory which contains prebuilt debs for specific model"
 	echo "--use-prebuilt-repo	Use prebuilt repository"
 	echo "--img-dir		Image generation directory"
