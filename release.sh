fabio-radin/build-artik

commit 904876fe6e92f7a73a8c8071b9ea3d84928b7f67

release: security: add check routine for restrictive security files exist
this patch adds checking restrictive files exist in release.sh, build_uboot.sh
and build_ubuntu.sh

Change-Id: I006fde08416e784dde5e745f088700a517202604
Signed-off-by: Jae-young Hwang <j-zero.hwang@samsung.com>

release.sh
@@ -121,6 +121,44 @@ parse_options()
 	done
 }
 
+check_restrictive_pkg()
+{
+	if [ "${TARGET_BOARD}" == "artik530s" ] || [ "${TARGET_BOARD}" == "artik533s" ] || [ "${TARGET_BOARD}" == "artik710s" ]; then
+		if [ "$SECURE_PREBUILT_DIR" != "" ]; then
+			cp -f $SECURE_PREBUILT_DIR/${TARGET_BOARD}_codesigner $PREBUILT_DIR
+			cp -f $SECURE_PREBUILT_DIR/secureos.img $PREBUILT_DIR
+
+			test ! -d $UBUNTU_MODULE_DEB_DIR && mkdir -p $UBUNTU_MODULE_DEB_DIR
+			cp -f $SECURE_PREBUILT_DIR/debs/*.deb $UBUNTU_MODULE_DEB_DIR
+		fi
+		RESTRICTIVE_PKG_LIST=`cat config/${TARGET_BOARD}_secure.list`
+		for l in $RESTRICTIVE_PKG_LIST
+		do
+			if [ $FULL_BUILD ]; then
+				if [ ! -f $l ]; then
+					echo -e "ERROR: cannot find ${l}-\e[0m"
+					echo -e "Build process has been terminated since the mandatory security binaries do not exist in your source code.\e[0m"
+					echo -e "Please download those files from artik.io with SLA agreement to continue to build.\e[0m"
+					echo -e "Once you download those files, please locate them to the following path."
+					echo -e ""
+					echo -e "1. secureos.img or fip-secure.img\e[0m"
+					echo -e "   copy to ../boot-firmwares-${TARGET_BOARD}/\e[0m"
+					echo -e "2. ${TARGET_BOARD}_codesigner"
+					echo -e "   copy to ../boot-firmwares-${TARGET_BOARD}/\e[0m"
+					echo -e "3. deb files\e[0m"
+					echo -e "   copy to ../ubuntu-build-service/prebuilt/${ARCH}/${TARGET_BOARD}/\e[0m"
+
+					exit 1
+				fi
+			else
+				if [ ! -f $l ] && [ "${l##*.}" == "deb" ]; then
+					continue
+				fi
+			fi
+		done
+	fi
+}
+
 package_check()
 {
 	command -v $1 >/dev/null 2>&1 || { echo >&2 "${1} not installed. Please install \"sudo apt-get install $2\""; exit 1; }
@@ -170,6 +208,8 @@ if [ "$CONFIG_FILE" != "" ]
 then
 	. $CONFIG_FILE
 fi
+
+check_restrictive_pkg
 
 if [ "$BUILD_DATE" == "" ]; then
 	BUILD_DATE=`date +"%Y%m%d.%H%M%S"`
@@ -240,7 +280,8 @@ if $FULL_BUILD ; then
 			--arch $BUILD_ARCH --chroot xenial-amd64-${BUILD_ARCH} \
 			--dest-dir $TARGET_DIR $SKIP_UBUNTU_BUILD \
 			--prebuilt-dir ../ubuntu-build-service/prebuilt/$BUILD_ARCH \
-			--img-dir $UBUNTU_IMG_DIR
+			--img-dir $UBUNTU_IMG_DIR \
+			-b ${BUILD_TARGET}
 	else
 		if [ "$FEDORA_PREBUILT_RPM_DIR" != "" ]; then
 			PREBUILD_ADD_CMD="-r $FEDORA_PREBUILT_RPM_DIR"

build_uboot.sh
@@ -138,7 +138,26 @@ gen_nexell_image()
 
 
 	if [ "$SECURE_BOOT" == "enable" ] && [ "$RSA_SIGN_TOOL" != "" ] ; then
+		chmod a+x ${RSA_SIGN_TOOL}
 		${RSA_SIGN_TOOL} -sign $TARGET_DIR/${output_file}
+	fi
+}
+
+check_rsa_sign_tool()
+{
+	if [ "${TARGET_BOARD}" == "artik530s" ] || [ "${TARGET_BOARD}" == "artik533s" ] || [ "${TARGET_BOARD}" == "artik710s" ]; then
+		test -e $SECURE_PREBUILT_DIR/${TARGET_BOARD}_codesigner && cp -f $SECURE_PREBUILT_DIR/${TARGET_BOARD}_codesigner ${RSA_SIGN_TOOL}
+		if [ ! -e ${RSA_SIGN_TOOL} ]; then
+			echo -e "\e[1;31mERROR: cannot find ${RSA_SIGN_TOOL}\e[0m"
+			echo -e "\e[1;31mBuild process has been terminated since the mandatory security binaries do not exist in your source code.\e[0m"
+			echo -e "\e[1;31mPlease download those files from artik.io with SLA agreement to continue to build.\e[0m"
+			echo -e "\e[1;31mOnce you download those files, please locate them to the following path.\e[0m"
+			echo -e ""
+			echo -e "\e[1;31m${TARGET_BOARD}_codesigner\e[0m"
+			echo -e "\e[1;31mcopy to ../boot-firmwares-${TARGET_BOARD}/\e[0m"
+
+			exit 1
+		fi
 	fi
 }
 
@@ -154,6 +173,8 @@ else
 		. $SCRIPT_DIR/config/$TARGET_BOARD.cfg
 	fi
 fi
+
+check_rsa_sign_tool
 
 test -d $TARGET_DIR || mkdir -p $TARGET_DIR
 

build_ubuntu.sh
@@ -14,6 +14,7 @@ PREBUILT_MODULE_DIR=
 IMG_DIR=
 UBUNTU_NAME=
 PREBUILT_REPO_DIR=
+TARGET_BOARD=
 
 print_usage()
 {
@@ -30,6 +31,7 @@ print_usage()
 	echo "--use-prebuilt-repo	Use prebuilt repository"
 	echo "--img-dir		Image generation directory"
 	echo "-n|--ubuntu-name	Ubuntu image name"
+	echo "-b [TARGET_BOARD]	Target board ex) -b artik710|artik530|artik5|artik10"
 	exit 0
 }
 
@@ -76,6 +78,9 @@ parse_options()
 				shift ;;
 			-n|--ubuntu-name)
 				UBUNTU_NAME="$2"
+				shift ;;
+			-b)
+				TARGET_BOARD="$2"
 				shift ;;
 			*)
 				shift ;;
@@ -186,6 +191,30 @@ find_unused_port()
 	done
 }
 
+restrictive_pkg_check()
+{
+	if [ "$SECURE_PREBUILT_DIR/debs" != "" ]; then
+		cp -f $SECURE_PREBUILT_DIR/debs/*.deb $DEST_DIR/debs
+	fi
+	if [ "${TARGET_BOARD}" == "artik530s" ] || [ "${TARGET_BOARD}" == "artik533s" ] || [ "${TARGET_BOARD}" == "artik710s" ]; then
+		RESTRICTIVE_PKG_LIST=`cat config/${TARGET_BOARD}_secure.list`
+		for l in $RESTRICTIVE_PKG_LIST
+		do
+			if [ "${l##*.}" == "deb" ] && [ ! -f $l ]; then
+				echo -e "\e[1;31mERROR: cannot find ${l}\e[0m"
+				echo -e "\e[1;31mBuild process has been terminated since the mandatory security binaries do not exist in your source code.\e[0m"
+				echo -e "\e[1;31mPlease download those files from artik.io with SLA agreement to continue to build.\e[0m"
+				echo -e "\e[1;31mOnce you download those files, please locate them to the following path.\e[0m"
+				echo -e ""
+				echo -e "\e[1;31mdeb files\e[0m"
+				echo -e "\e[1;31mcopy to ../ubuntu-build-service/prebuilt/${ARCH}/${TARGET_BOARD}/\e[0m"
+
+				exit 1
+			fi
+		done
+	fi
+}
+
 trap abnormal_exit INT ERR
 
 package_check sbuild sponge python3
@@ -201,6 +230,8 @@ fi
 if [ "$PREBUILT_REPO_DIR" != "" ]; then
 	cp -rf $PREBUILT_REPO_DIR/* $DEST_DIR/debs
 fi
+
+restrictive_pkg_check
 
 start_local_server $DEST_DIR/debs $PORT
 

mksdboot.sh
@@ -39,6 +39,7 @@ gen_nexell_image_mon()
 		input_file=bl_mon.img
 
 		if [ "$RSA_SIGN_TOOL" != "" ]; then
+			chmod a+x ${RSA_SIGN_TOOL}
 			${RSA_SIGN_TOOL} -sign $TARGET_DIR/${input_file}
 		fi
 	fi
@@ -54,6 +55,7 @@ gen_nexell_image_secure()
 	fi
 
 	if [ "$RSA_SIGN_TOOL" != "" ]; then
+		chmod a+x ${RSA_SIGN_TOOL}
 		${RSA_SIGN_TOOL} -sign $TARGET_DIR/${input_file}
 	fi
 }

commit 48820bc17052fca058c1ca2b31cf55f72313c918

release: fix secureos filename for artik710s
Fixes: 904876fe6e92 ("release: security: add check routine for
		       restrictive security files exist")

This patch fixes invalid secure os file name for artik710s. Unlike
artik530 series, the artik710s uses the fip-secure.img name.

Change-Id: I5ca7a5f9506a8924861e9ac96e95d5cebd34154c
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

config/artik530s.cfg
@@ -9,6 +9,7 @@ export SECURE_PREBUILT_DIR=$ARTIK_BUILD_DIR/../security-firmwares-artik530s
 export INITRD=$PREBUILT_DIR/$RAMDISK_NAME
 export SECURE_BOOT=enable
 export SECURE_OS=trustware
+export SECURE_OS_FILE=secureos.img
 export RSA_SIGN_TOOL=$PREBUILT_DIR/artik530s_codesigner
 export FEDORA_PREBUILT_RPM_DIR=$PREBUILT_DIR/rpms
 

config/artik533s.cfg
@@ -9,6 +9,7 @@ export SECURE_PREBUILT_DIR=$ARTIK_BUILD_DIR/../security-firmwares-artik533s
 export INITRD=$PREBUILT_DIR/$RAMDISK_NAME
 export SECURE_BOOT=enable
 export SECURE_OS=trustware
+export SECURE_OS_FILE=secureos.img
 export RSA_SIGN_TOOL=$PREBUILT_DIR/artik533s_codesigner
 export FEDORA_PREBUILT_RPM_DIR=$PREBUILT_DIR/rpms
 

config/artik710s.cfg
@@ -8,6 +8,7 @@ export PREBUILT_DIR=$ARTIK_BUILD_DIR/../boot-firmwares-artik710s
 export SECURE_PREBUILT_DIR=$ARTIK_BUILD_DIR/../security-firmwares-artik710s
 export INITRD=$PREBUILT_DIR/$RAMDISK_NAME
 export SECURE_BOOT=enable
+export SECURE_OS_FILE=fip-secure.img
 
 export FIP_SEC_LOAD_ADDR=0x7fb00000
 

release.sh
@@ -136,7 +136,7 @@ check_restrictive_pkg()
 	if [ "${TARGET_BOARD}" == "artik530s" ] || [ "${TARGET_BOARD}" == "artik533s" ] || [ "${TARGET_BOARD}" == "artik710s" ]; then
 		if [ "$SECURE_PREBUILT_DIR" != "" ]; then
 			cp -f $SECURE_PREBUILT_DIR/${TARGET_BOARD}_codesigner $PREBUILT_DIR
-			cp -f $SECURE_PREBUILT_DIR/secureos.img $PREBUILT_DIR
+			cp -f $SECURE_PREBUILT_DIR/${SECURE_OS_FILE} $PREBUILT_DIR
 
 			test ! -d $UBUNTU_MODULE_DEB_DIR && mkdir -p $UBUNTU_MODULE_DEB_DIR
 			cp -f $SECURE_PREBUILT_DIR/debs/*.deb $UBUNTU_MODULE_DEB_DIR