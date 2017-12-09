fabio-radin/build-artik

commit a9658383371a4c4a5bab98c4df6807dd0d03762f

sysroot: change symlink path from absolute to relative
This patch changes symbolic link in sysroot with absolute path
into relative path. The absolute path in sysroot is wrong because
it indicates host files. This patch fixes it.

Change-Id: If942e3fa538838df32ae03887f13df91a67cacc7
Signed-off-by: Sowon Na <sowon.na@samsung.com>

build_ubuntu_sysroot.sh
@@ -186,6 +186,28 @@ MODEL=${upper_model}
 __EOF__
 }
 
+change_symlink_path()
+{
+	pushd $TARGET_DIR
+	symlink_sysroot=$(sudo find rootfs/ -type l -lname '/*')
+
+	for file in $symlink_sysroot
+	do
+		link=$(ls -l $file | awk '{print $11}')
+		tmp=(`echo $file | tr "/" "\n"`)
+		i=$((${#tmp[@]}-2))
+		if [ $i -eq 0 ]; then
+			link='.'$link
+		elif [ $i -gt 0 ]; then
+			link='..'$link
+		fi
+		link=$(seq -s "`echo -e ''`" $(($i-1)) | sed 's/./\.\.\//g')$link
+		sudo rm -rf $file
+		sudo ln -s $link $file
+	done
+	popd
+}
+
 trap abnormal_exit INT ERR
 trap 'error ${LINENO} ${?}' ERR
 
@@ -283,6 +305,8 @@ mkdir -p $TARGET_DIR/rootfs
 sudo tar zxf $TARGET_DIR/${UBUNTU_NAME}.tar.gz -C $TARGET_DIR/rootfs
 sudo mv $TARGET_DIR/artik_sysroot_release $TARGET_DIR/rootfs/
 sync
+
+change_symlink_path
 
 sudo tar zcf $TARGET_DIR/${UBUNTU_NAME}.tar.gz -C $TARGET_DIR/rootfs .
 sudo tar --exclude=usr/lib/python2.7 --exclude=usr/lib/python3.5 -zcf $TARGET_DIR/${UBUNTU_NAME}-IDE.tar.gz -C $TARGET_DIR/rootfs usr/include usr/lib lib

commit 5ca16061fab106dfcfea866d7bc1067d2f5b111d

sysroot: fix link name error on stress condition
Fixes: a965838 ("sysroot: change symlink path from absolute to relative")

This patch fixes link name error on stress condition that depth
is more than 10. If the directory depth is more than 10,
seq make double digits of it. This patch fixes it.

Change-Id: I7b35e2bdc00f7ccc9758bc99be859d41a7baa5f1
Signed-off-by: Sowon Na <sowon.na@samsung.com>

build_ubuntu_sysroot.sh
@@ -201,7 +201,7 @@ change_symlink_path()
 		elif [ $i -gt 0 ]; then
 			link='..'$link
 		fi
-		link=$(seq -s "`echo -e ''`" $(($i-1)) | sed 's/./\.\.\//g')$link
+		link=$(seq -s../ $i | tr -d '[:digit:]')$link
 		sudo rm -rf $file
 		sudo ln -s $link $file
 	done