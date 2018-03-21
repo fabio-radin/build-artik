fcdd4632a6e487c978fb5744962f0d78c61b3e0a
Heesub Shin <heesub.shin@samsung.com>
2018-03-21 15:42:52 +0900

    config: introduce a new variable, FIT_IMAGE
    This commit adds a new variable, FIT_IMAGE. It specifies the file name
    of FIT which has been hardcoded in 'rsa_kernel.fit' so far. This change
    will make the FIT file name configurable.
    
    Change-Id: Id8cb12f67b5a9948bdb9c189b0ffd06adb5e33bd
    Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

--- mkvboot.sh
+++ mkvboot.sh
@@ -15,11 +15,11 @@ dd if=$INITRD of=$OUTPUT_DIR/initrd.gz bs=1 skip=64
 
 cp $ITS_FILE $OUTPUT_DIR
 pushd $OUTPUT_DIR
-./mkimage -f $ITS_NAME rsa_kernel.fit
-./mkimage -k $KEY_DIR -r -F -K u-boot.dtb rsa_kernel.fit
+./mkimage -f $ITS_NAME $FIT_IMAGE
+./mkimage -k $KEY_DIR -r -F -K u-boot.dtb $FIT_IMAGE
 
 # Copy verified boot files to original files
-cp rsa_kernel.fit zImage
+cp $FIT_IMAGE zImage
 cat u-boot.bin u-boot.dtb > u-boot-dtb.bin
 cp params_vboot.bin params.bin
 cp params_recovery_vboot.bin params_recovery.bin
