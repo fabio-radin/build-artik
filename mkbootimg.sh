fabio-radin/build-artik

commit d85cb1abd3410bf0e2d0b0db60d5702386716cd1

mkbootimg: populate fit image when verified boot is enabled
When the verified boot is enabled, populate fit image into the bootimg.
Please note that we also copy the ramdisk image as ARTIK-520 still needs
it. We should fix this later.

Change-Id: I9806f16bee2fd3f1dc8b9e5fae6b952508903198
Signed-off-by: Heesub Shin <heesub.shin@samsung.com>

mkbootimg.sh
@@ -47,9 +47,15 @@ install_boot_image()
 	test -d mnt || mkdir mnt
 	sudo mount -o loop boot.img mnt
 
-	sudo sh -c "install -m 664 $TARGET_DIR/$KERNEL_IMAGE mnt"
-	sudo sh -c "install -m 664 $TARGET_DIR/$KERNEL_DTB mnt"
-	sudo sh -c "install -m 664 $TARGET_DIR/$RAMDISK_NAME mnt"
+	if [ "$VERIFIED_BOOT" == "false" ]; then
+		sudo sh -c "install -m 664 $TARGET_DIR/$KERNEL_IMAGE mnt"
+		sudo sh -c "install -m 664 $TARGET_DIR/$KERNEL_DTB mnt"
+		sudo sh -c "install -m 664 $TARGET_DIR/$RAMDISK_NAME mnt"
+	else
+		sudo sh -c "install -m 664 $TARGET_DIR/$FIT_IMAGE mnt"
+		# only for ARTIK-520 which FIT does not contain the ramdisk (should be removed)
+		sudo sh -c "install -m 664 $TARGET_DIR/$RAMDISK_NAME mnt"
+	fi
 
 	if [ "$OVERLAY" == "true" ]; then
 		test -d mnt/overlays || sudo mkdir mnt/overlays

release.sh
@@ -245,6 +245,7 @@ export BUILD_DATE=$BUILD_DATE
 export BUILD_VERSION=$BUILD_VERSION
 
 export TARGET_DIR=$TARGET_DIR/$BUILD_VERSION/$BUILD_DATE
+export VERIFIED_BOOT=$VERIFIED_BOOT
 
 sudo ls > /dev/null 2>&1
 

commit 4668ddeaa713b0d1b264efe104e626e477bd8556

mkbootimg: fix VERIFIED_BOOT variable comparison
Fixes: d85cb1abd341 ("mkbootimg: populate fit image when verified boot
is enabled")

When we enabled the verified boot feature for kernel build, the
VERIFIED_BOOT variable can be defined by release.sh script. However,
the problem can be occurred when we're using the script alone without
the release.sh script. To fix this invalid comparison, we just compare
the variable "$VERIFIED_BOOT" == "true" at first. This patch also adds
--vboot option to enable the feature manually.

Change-Id: Ib3f8763d4892c53aeda304a00819481fb0d5f258
Signed-off-by: Chanho Park <chanho61.park@samsung.com>

mkbootimg.sh
@@ -21,6 +21,9 @@ parse_options()
 			-b)
 				TARGET_BOARD="$2"
 				shift ;;
+			--vboot)
+				VERIFIED_BOOT="true"
+				shift ;;
 		esac
 	done
 }
@@ -42,13 +45,13 @@ install_boot_image()
 {
 	test -d mnt || mkdir mnt
 
-	if [ "$VERIFIED_BOOT" == "false" ]; then
-		install -m 664 $TARGET_DIR/$KERNEL_IMAGE mnt
-		install -m 664 $TARGET_DIR/$KERNEL_DTB mnt
-		install -m 664 $TARGET_DIR/$RAMDISK_NAME mnt
-	else
+	if [ "$VERIFIED_BOOT" == "true" ]; then
 		install -m 664 $TARGET_DIR/$FIT_IMAGE mnt
 		# only for ARTIK-520 which FIT does not contain the ramdisk (should be removed)
+		install -m 664 $TARGET_DIR/$RAMDISK_NAME mnt
+	else
+		install -m 664 $TARGET_DIR/$KERNEL_IMAGE mnt
+		install -m 664 $TARGET_DIR/$KERNEL_DTB mnt
 		install -m 664 $TARGET_DIR/$RAMDISK_NAME mnt
 	fi
 